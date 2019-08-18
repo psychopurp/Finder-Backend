a0ad7320f3d6ae667c53774460c3337b184ea16b
Tobias Yin <yrt1999@163.com>
2019-08-18 20:23:40 +0800

    Add utils and new topics view.

--- backend/apps/topic/views.py
+++ backend/apps/topic/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.http import JsonResponse
 
-# Create your views here.
+from topic.models import Topic
+from util.method_util import request_method_check
+from util.model_util import get_result_by_query_page, str_page_to_int
+
+
+@request_method_check("GET")
+def get_topics(request):
+    query = Q(title__contains=request.GET.get("query"))
+    return JsonResponse(get_result_by_query_page(Topic, query, str_page_to_int(request.GET.get("page"))))
--- backend/util/model_util.py
+++ backend/util/model_util.py
@@ -4,6 +4,8 @@
 import random
 import time
 
+from django.db import models
+
 
 def get_time():
     return str(int(time.time()))
@@ -35,6 +37,44 @@ def model_to_dict(model, props=None) -> dict:
     return result_dict
 
 
+def get_result_by_query_page(model, query=None, page=0, props=None) -> dict:
+    """
+    可直接对模型获取并分页
+    :param model: 需要获取的模型
+    :param query: 需要获取的查询方法, 需要是一个Q对象
+    :param page: 需要的页数
+    :param props: 需要获取的字段, 遵循和model_to_dict同样规则
+    :return: 返回获取的后的dict
+    """
+    if not query:
+        data = model.objects.all()
+    else:
+        data = model.objects.filter(query)
+    result, total_page = get_query_set_by_page(data, page)
+    return {"data": query_set_to_list(result, props), "total_page": total_page, "status": True}
+
+
+def get_query_set_by_page(query, page: int, page_size: int = 10) -> (all, int):
+    """
+    获取分页后的query_set
+    :param query: 待分页的序列
+    :param page: 需要的页数
+    :param page_size: 每一页的大小
+    :return: 返回值第一个为分页后的结果, 第二个是总页数
+    """
+    page = int(page)
+    total_page = len(query) / page_size
+    if total_page % 1 > 0:
+        # 如果未填满一页的条, 按一页算
+        total_page += 1
+    total_page = int(total_page)
+    if page + 1 > total_page:
+        # 如果页数过多, 返回空的queryset
+        return query[0:0], total_page
+        # 如果页数正常, 返回结果
+    return query[page * page_size: (page + 1) * page_size], total_page
+
+
 def query_set_to_list(query_set, props=None) -> list:
     """
     将query_set转换成dict组成的列表
@@ -49,7 +89,12 @@ def query_set_to_list(query_set, props=None) -> list:
     return [model_to_dict(i, props) for i in query_set]
 
 
-def generate_random_str(length=32):
+def generate_random_str(length=32) -> str:
+    """
+    生成随机字符串
+    :param length: 字符串长度
+    :return: 生成的字符串
+    """
     str_token = []
     for char in range(ord('0'), ord('9') + 1):
         str_token.append(chr(char))
@@ -61,3 +106,15 @@ def generate_random_str(length=32):
     for _ in range(length):
         result_str += str_token[random.randint(0, len(str_token) - 1)]
     return result_str
+
+
+def str_page_to_int(page: str) -> int:
+    if not page:
+        page = 0
+    try:
+        page = int(page)
+    except TypeError as e:
+        page = 0
+    except ValueError as e:
+        page = 0
+    return page
