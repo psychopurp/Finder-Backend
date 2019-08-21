bf58b7f431bfc7dd198ddb04c8686e3037ac06fc
Daisy Fu <ouluoya@qq.com>
2019-08-21 10:29:13 +0800

    Fix the problem of two same foreignkey.

--- backend/apps/dialog/models.py
+++ backend/apps/dialog/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # Create your models here.
 class Dialog(models.Model):
     sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name='receiver', on_delete=models.CASCADE)
     time = models.DateTimeField(default=datetime.now)
     content = models.TextField()
 
@@ -15,4 +15,4 @@ class Dialog(models.Model):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.sender + '向' + self.receiver + '发送消息'
+        return self.sender.__str__() + '向' + self.receiver.__str__() + '发送消息'
--- backend/apps/recruit/admin.py
+++ backend/apps/recruit/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from recruit.models import RecruitType, Recruit, CandidateStatus, Candidate
+from recruit.models import RecruitType, Recruit, Candidate
 
 # Register your models here.
 admin.site.register(RecruitType)
 admin.site.register(Recruit)
-admin.site.register(CandidateStatus)
 admin.site.register(Candidate)
